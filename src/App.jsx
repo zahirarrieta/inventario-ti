97c41e08d364defe588c39f1f35f8b662afa25c1
ccguerreroc <cc.guerreroc@lanuevaamerica.edu.co>
2026-08-19 12:29:42 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+﻿function App() {
+    return (
+        <div>
+            <h1>Inventario TI</h1>
+            <p>Aplicación iniciada correctamente.</p>
+        </div>
+    );
+}
+
+export default App;
